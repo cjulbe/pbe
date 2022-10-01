93e77586441185a2e558a0c4f38f148b64958380
Carlota Julbe <113508015+cjulbe@users.noreply.github.com>
2022-10-01 19:23:54 +0200

    v3.2 fixed bugs

--- ReadID.py
+++ ReadID.py
@@ -1,26 +1,22 @@
 #!/usr/bin/env python
-from time import sleep
-import sys
+import os
 import RPi.GPIO as GPIO
-from mfrc522 import SimpleMFRC522
+from mfrc522 import *
 
 class Rfid_RC522:
     # return uid in hex str 
     def read_uid(self):
            reader= SimpleMFRC522()
-           GPIO.cleanup()
-           id,text = reader.read()
-           id_hex= hex(int(id)).upper()
+           id= reader.read_id()
+           id_hex= hex(id).upper()
            return id_hex
 
 if __name__ == "__main__":
     try:
-        while True:
-            print("Hold a tag near the reader")
+            print("Hold your card near the reader")
             rf= Rfid_RC522()
             uid= rf.read_uid()
             print("ID: %s\n" % (uid[2:10]))
-            sleep(5)
     except KeyboardInterrupt:
             GPIO.cleanup()
             raise
