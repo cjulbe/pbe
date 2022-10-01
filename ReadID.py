2892736a964a896465c90675d2cf76b31f5d9ee6
Carlota Julbe <113508015+cjulbe@users.noreply.github.com>
2022-10-01 16:58:45 +0200

    v1.0
    Conversion decimal to hexadecimal format missing

---
+++ ReadID.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from time import sleep
+import sys
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+reader = SimpleMFRC522()
+GPIO.cleanup()
+
+try:
+    while True:
+        print("Hold a tag near the reader")
+        id, text = reader.read()
+         print("ID: %s\nText: %s" % (id,text))
+        sleep(5)
+except KeyboardInterrupt:
+    GPIO.cleanup()
+    raise
+
+
+
