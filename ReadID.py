bafbe507a6c9f5e4f1414d571f57f65e2826fa72
Carlota Julbe <113508015+cjulbe@users.noreply.github.com>
2022-10-01 18:26:58 +0200

    v3.0 Code adapted to the template given for the project

--- ReadID.py
+++ ReadID.py
@@ -3,16 +3,24 @@ from time import sleep
 import sys
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
-reader = SimpleMFRC522()
-GPIO.cleanup()
 
-try:
-    while True:
-        print("Hold a tag near the reader")
-        id,text = reader.read()
-        id_hex= hex(int(id))
-        print("ID: %s\n" % (id_hex[2:10]).upper())
-        sleep(5)
-except KeyboardInterrupt:
-    GPIO.cleanup()
-    raise
+class Rfid_RC522:
+    # return uid in hex str 
+    def read_uid(self):
+           reader= SimpleMFRC522()
+           GPIO.cleanup()
+           id,text = reader.read()
+           id_hex= hex(int(id)).upper()
+           return id_hex
+
+if __name__ == "__main__":
+    try:
+        while True:
+            print("Hold a tag near the reader")
+            rf= Rfid_RC522()
+            uid= rf.read_uid()
+            print("ID: %s\n" % (uid[2:10]))
+            sleep(5)
+    except KeyboardInterrupt:
+            GPIO.cleanup()
+            raise
