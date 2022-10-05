21001ea6c302d6f6b52f53e7500c2c0e9f96033e
Carlota Julbe <113508015+cjulbe@users.noreply.github.com>
2022-10-05 09:11:44 +0200

    v4.0: Define constructor

--- Puzzle1/Read_UID.py
+++ Puzzle1/Read_UID.py
@@ -4,19 +4,20 @@ import RPi.GPIO as GPIO
 from mfrc522 import *
 
 class Rfid_RC522:
+    # define constructor
+    reader= SimpleMFRC522()
     # return uid in hex str 
     def read_uid(self):
-           reader= SimpleMFRC522()
            id= reader.read_id()
            id_hex= hex(id).upper()
-           return id_hex
+           return id_hex[2:10]
 
 if __name__ == "__main__":
     try:
             print("Hold your card near the reader")
             rf= Rfid_RC522()
             uid= rf.read_uid()
-            print("UID: %s\n" % (uid[2:10]))
+            print("UID: %s\n" % (uid))
     except KeyboardInterrupt:
             GPIO.cleanup()
             raise
